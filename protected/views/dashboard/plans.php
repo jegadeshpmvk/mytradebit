847da4b4fef3b117acb3678516e3906901a356e5
Jegadesh <jegadesh@triumphal.io>
2023-07-09 18:58:24 +0530

    Update profile

--- protected/views/dashboard/plans.php
+++ protected/views/dashboard/plans.php
@@ -2,7 +2,26 @@
     <div class="dash_sec dash_sec_padd">
         <div class="c">
             <div class="dash_sec_inner">
-                <div class="">
+                <div class="plan_title text_color_gradiant align_center">Choose your plan</div>
+
+                <div class="plans_content">
+                    <div class="plans_header">
+                        <div class="_col _col_4">FEATURES</div>
+                        <div class="_col _col_4">BASIC</div>
+                        <div class="_col _col_4">PRO (LIVE ANALYSIS)</div>
+                        <div class="_col _col_4">PRO + <br />
+                            (LIVE + HISTORICAL ANALYSIS)</div>
+                    </div>
+                    <div class="plans_body">
+                        <div class="_col _col_4">
+                            
+                        </div>
+                        <div class="_col _col_4">BASIC</div>
+                        <div class="_col _col_4">PRO (LIVE ANALYSIS)</div>
+                        <div class="_col _col_4">PRO + <br />
+                            (LIVE + HISTORICAL ANALYSIS)</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
