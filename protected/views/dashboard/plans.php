2aaad58def138026eaf2436c4250fb7773470d84
Jegadesh <jegadesh@triumphal.io>
2023-07-09 23:35:39 +0530

    Update profile

--- protected/views/dashboard/plans.php
+++ protected/views/dashboard/plans.php
@@ -2,7 +2,7 @@
     <div class="dash_sec dash_sec_padd">
         <div class="c">
             <div class="dash_sec_inner">
-                <div class="plan_title text_color_gradiant align_center">Choose your plan</div>
+                <div class="plans_title text_color_gradiant align_center">Choose your plan</div>
 
                 <div class="plans_content">
                     <div class="plans_header">
@@ -14,12 +14,154 @@
                     </div>
                     <div class="plans_body">
                         <div class="_col _col_4">
-                            
+                            <div class="plan_title">Price</div>
                         </div>
-                        <div class="_col _col_4">BASIC</div>
-                        <div class="_col _col_4">PRO (LIVE ANALYSIS)</div>
-                        <div class="_col _col_4">PRO + <br />
-                            (LIVE + HISTORICAL ANALYSIS)</div>
+                        <div class="_col _col_4">
+                            <div class="plan_title">Free</div>
+                            <div class="plan_sub_title">(3 days trial - Pro Live)</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_title"><del>₹1200</del> <span>₹ 400 /</span> <sup>month</sup></div>
+                            <div class="plan_sub_title">(Limited time period offer)</div>
+                            <div class="plan_sub_title">(Incl. 18% GST)</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_title"><del>₹3200</del> <span>₹ 1600 /</span> <sup>month</sup></div>
+                            <div class="plan_sub_title">(Limited time period offer)</div>
+                            <div class="plan_sub_title">(Incl. 18% GST)</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row align_left">Global Sentiment</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row align_left">FII - DII Sentiment</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row align_left">Sectorwise Performance (Pre market + Live Market)</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row align_left">Live Heat Map</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row align_left">Market Pulse</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row">Only Nifty 50</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row align_left">Market Cheat Sheet</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row">Limited Scrips Only</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row align_left">Options Board</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row">---</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row align_left">Intraday Setups</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row">---</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row align_left">Positional Setups</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row">---</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row align_left">FII - DII Data</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row">---</div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4">
+                            <div class="plan_row"><img src="<?= Yii::getAlias('@icons') ?>/tick.png"></div>
+                        </div>
+                        <div class="_col _col_4 ex_padd">
+                            <div class="plan_row opa"><a class="btn btn_blue">Get Pros</a></div>
+                        </div>
+                        <div class="_col _col_4 ex_padd">
+                            <div class="plan_row opa"><a class="btn btn_blue">Get Pros</a></div>
+                        </div>
+                        <div class="_col _col_4 ex_padd">
+                            <div class="plan_row"><a class="btn btn_blue">Get Pros</a></div>
+                        </div>
+                        <div class="_col _col_4 ex_padd">
+                            <div class="plan_row"><a class="btn btn_blue">Get Ultra Pro</a></div>
+                        </div>
                     </div>
                 </div>
             </div>
