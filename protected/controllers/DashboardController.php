f73121021ddd17cafd0b62f5b44d42ff436b28f6
Jegadesh <jegadesh@triumphal.io>
2023-08-11 00:44:13 +0530

    Market pulse

--- protected/controllers/DashboardController.php
+++ protected/controllers/DashboardController.php
@@ -20,18 +20,21 @@ class DashboardController extends Controller
     {
         $this->setupMeta([], 'Dashboard');
         $details =  FiiDii::find()->active()->orderBy(['date' => SORT_DESC])->one();
-        $max_value = max([$details->stocks_fii, $details->stocks_dii]);
-
-        if ($max_value < 1000) {
-            $amount = 2000;
-        } else {
-            $amount = 4000;
+        $pre_marketdata = $this->getPreMarketData();
+        $results= [];
+        if(!empty($pre_marketdata->data)) {
+            foreach($pre_marketdata->data as $k => $d) {
+                echo '<pre>';
+                print_r($d);exit;
+                $results[''] = '';
+            }
         }
         return $this->render('index', [
             'getGlobalSentiments' => $this->getGlobalSentiments(),
             'details' => [$details->stocks_fii, $details->stocks_dii],
             'stocks_sentiment' => $details->stocks_sentiment,
-            'date' => @$details->date ? date('d M y', @$details->date) : '---'
+            'date' => @$details->date ? date('d M y', @$details->date) : '---',
+            'date' => $this->getPreMarketData(),
         ]);
     }
 
@@ -566,6 +569,24 @@ class DashboardController extends Controller
         $curl = curl_init();
         curl_setopt_array($curl, [
             CURLOPT_URL => 'https://groww.in/v1/api/stocks_data/v1/global_instruments?instrumentType=GLOBAL_INSTRUMENTS',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET'
+        ]);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($response, true);
+    }
+
+    protected function getPreMarketData()
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => 'https://www.nseindia.com/api/allIndices',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
