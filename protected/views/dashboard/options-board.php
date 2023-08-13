7040b3dc1e6a28f4fdae65aa2158063eefea25e1
Jegadesh <jegadesh@triumphal.io>
2023-08-13 13:30:36 +0530

    Options board

---
+++ protected/views/dashboard/options-board.php
@@ -0,0 +1,185 @@
+<div class="dashboard">
+    <div class="dash_sec dash_sec_padd">
+        <div class="c">
+            <div class="dash_sec_inner">
+                <div class="intra_title stocks_type_groups">
+                    <label>
+                        <input class="stocks_type" name="stocks_type" type="radio" checked value="">
+                        <span class="pre_market_radio"></span> <span>NIFTY - 18618.25</span>
+                    </label>
+                    <label>
+                        <input class="stocks_type" name="stocks_type" type="radio" checked value="">
+                        <span class="pre_market_radio"></span> <span>BANKNIFTY - 44132.10</span>
+                    </label>
+                    <div class="options_form_group">
+                        <label>Trade Date</label>
+                        <input type="date" name="trade_date" class="options_form_control" />
+                    </div>
+                    <div class="options_form_group">
+                        <label>Expiry Date</label>
+                        <input type="date" name="expiry_date" class="options_form_control" />
+                    </div>
+                    <div class="options_form_group">
+                        <label>Start Time</label>
+                        <input type="text" name="start_time" class="options_form_control" />
+                    </div>
+                    <div class="options_form_group">
+                        <label>End Time</label>
+                        <input type="text" name="end_time" class="options_form_control" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="dash_sec dash_sec_padd">
+        <div class="c">
+            <div class="dash_sec_inner">
+                <div class=""></div>
+                <div class="timings">
+                    <label>
+                        <input class="timing" name="stocks_type" type="radio" checked value="">
+                        <span class="timing_button">Last 5 mins</span>
+                    </label>
+                    <label>
+                        <input class="timing" name="stocks_type" type="radio" value="">
+                        <span class="timing_button">Last 10 mins</span>
+                    </label>
+                    <label>
+                        <input class="timing" name="stocks_type" type="radio" value="">
+                        <span class="timing_button">Last 15 mins</span>
+                    </label>
+                    <label>
+                        <input class="timing" name="stocks_type" type="radio" value="">
+                        <span class="timing_button">Last 30 mins</span>
+                    </label>
+                    <label>
+                        <input class="timing" name="stocks_type" type="radio" value="">
+                        <span class="timing_button">Last 1 Hrs</span>
+                    </label>
+                    <label>
+                        <input class="timing" name="stocks_type" type="radio" value="">
+                        <span class="timing_button">Last 2 Hrs</span>
+                    </label>
+                    <label>
+                        <input class="timing" name="stocks_type" type="radio" value="">
+                        <span class="timing_button">Last 3 Hrs</span>
+                    </label>
+                    <label>
+                        <input class="timing" name="stocks_type" type="radio" value="">
+                        <span class="timing_button">Full Day</span>
+                    </label>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="dash_sec dash_sec_padd">
+        <div class="c">
+            <div class="intra_row">
+                <div class="_col _option_scope">
+                    <div class="dash_sec_inner">
+                        <div class="intra_title">
+                            <span class="">Options Scope <span title="test">(?)</span></span>
+                        </div>
+                        <div class="dash_content">
+                            <table class="custom_table_data">
+                                <thead>
+                                    <tr>
+                                        <th>Calls</th>
+                                        <th>Strike</th>
+                                        <th>Puts</th>
+                                        <th>Level Analysis</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="pre_market_data">
+                                    <tr>
+                                        <td>Call Unwinding</td>
+                                        <td>18,200</td>
+                                        <td>Put Unwinding</td>
+                                        <td>Strong Support</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Call Unwinding</td>
+                                        <td>18,200</td>
+                                        <td>Put Unwinding</td>
+                                        <td>Strong Support</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Call Unwinding</td>
+                                        <td>18,200</td>
+                                        <td>Put Unwinding</td>
+                                        <td>Strong Support</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Call Unwinding</td>
+                                        <td>18,200</td>
+                                        <td>Put Unwinding</td>
+                                        <td>Strong Support</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Call Unwinding</td>
+                                        <td>18,200</td>
+                                        <td>Put Unwinding</td>
+                                        <td>Strong Support</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Call Unwinding</td>
+                                        <td>18,200</td>
+                                        <td>Put Unwinding</td>
+                                        <td>Strong Support</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Call Unwinding</td>
+                                        <td>18,200</td>
+                                        <td>Put Unwinding</td>
+                                        <td>Strong Support</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Call Unwinding</td>
+                                        <td>18,200</td>
+                                        <td>Put Unwinding</td>
+                                        <td>Strong Support</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Call Unwinding</td>
+                                        <td>18,200</td>
+                                        <td>Put Unwinding</td>
+                                        <td>Strong Support</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Call Unwinding</td>
+                                        <td>18,200</td>
+                                        <td>Put Unwinding</td>
+                                        <td>Strong Support</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Call Unwinding</td>
+                                        <td>18,200</td>
+                                        <td>Put Unwinding</td>
+                                        <td>Strong Support</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Call Unwinding</td>
+                                        <td>18,200</td>
+                                        <td>Put Unwinding</td>
+                                        <td>Strong Support</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Call Unwinding</td>
+                                        <td>18,200</td>
+                                        <td>Put Unwinding</td>
+                                        <td>Strong Support</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="_col _option_scope_center">
+                </div>
+                <div class="_col _option_scope">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
